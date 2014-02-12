d9611e5c2de27714b43b0d0813afa47c13ebd6b5
Stefan Hagen <stefan@stefanhagen.nl>
2014-02-12 16:02:31 +0100

    Remove the now ignored secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Website::Application.config.secret_token = '2e9a2fdc5e39b2917d7d6464cc78d94d490ca2c8c9fabd4373b22330049e1524d741a4e4db331fa320bab14c3ae18fcc5e92e8024c604091ef5cbb7086050ad3'
